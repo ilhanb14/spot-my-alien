6bddae5bbb08e829591458b700b58545ceadada8
ilhanb14 <ilhan.bayik@cursist.syntrapxl.be>
2025-05-28 12:28:32 +0200

    Removed image_url column

--- database/migrations/2025_05_28_102739_add_featured_to_sightings_table.php
+++ database/migrations/2025_05_28_102739_add_featured_to_sightings_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
     {
         Schema::table('sightings', function (Blueprint $table) {
             $table->boolean('is_featured')->default(false);
-            $table->string('image_url')->nullable();
         });
     }
 
